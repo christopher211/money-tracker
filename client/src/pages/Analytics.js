c546b6da68935d40669959c855b1d057ada2161e
clayandemar <clayandemar@gmail.com>
2023-03-15 11:41:23 -0500

    Chart

--- client/src/pages/Analytics.js
+++ client/src/pages/Analytics.js
@@ -2,6 +2,7 @@ import { Wrapper } from "../components";
 import Yearly from "../components/AnalyticsPage/Yearly"
 import Monthly from "../components/AnalyticsPage/Monthly"
 import Expenses from "../components/AnalyticsPage/Expenses"
+import Chart from "../components/AnalyticsPage/Chart"
 import Navigation from "../components/Navigation"
 
 const Analytics = (props) => {
@@ -10,10 +11,8 @@ const Analytics = (props) => {
     <Wrapper>
       <h1>Analytics</h1>
       <div class="flexbox-container">
-        <Navigation  class="flexbox-item"/>
-        <Yearly class="flexbox-item flexbox-item-1"/>
-        <Monthly class="flexbox-item flexbox-item-2"/>
-        <Expenses class="flexbox-item flexbox-item-3"/>
+        <Navigation class="flexbox-item" />
+        <Chart />
       </div>
     </Wrapper>
   )
